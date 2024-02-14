c40782cdc288808540a82dd8a5f3ae25fc67e9bc
Alparslan Tarkan <atarkan@hotmail.com>
2024-02-14 23:39:50 +0100

    The Third Party implemented and works as expected

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import SearchInput from "./SearchInput";
+import axios from "axios";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { images: [] };
     this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
 
-  onSearchSubmit(entry) {
-    console.log(entry);
+  async onSearchSubmit(entry) {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=42379062-467261a752f3f4ccd20bf0180&q=${entry}&image_type=photo&pretty=true`
+    );
+    this.setState({ images: response.data.hits });
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "30px" }}>
         <SearchInput onSearchSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
